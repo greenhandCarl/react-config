0a587e215b728eff4357256d3d0d15a615a7f17e
zhengjin <zhengjin@yatang.com>
2017-10-23 11:57:27 +0800

    增加sass-loader

---
+++ src/components/AddTodo.js
@@ -0,0 +1,26 @@
+import React, { Component, PropTypes } from 'react';
+import './addTodo.scss';
+
+export default class AddTodo extends Component {
+  render() {
+    return (
+      <div className="add_input">
+        <input type='text' ref='input' />
+        <button className="btn" onClick={(e) => this.handleClick(e)}>
+          Add
+        </button>
+      </div>
+    )
+  }
+
+  handleClick(e) {
+    const node = this.refs.input
+    const text = node.value.trim()
+    this.props.onAddClick(text)
+    node.value = ''
+  }
+}
+
+AddTodo.propTypes = {
+  onAddClick: PropTypes.func.isRequired
+}
